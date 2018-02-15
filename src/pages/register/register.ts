72edb4c05933eb3f85ee3f8519459fbc509672f2
fayiz7 <fayiz7.kau@gmail.com>
2018-02-15 12:38:53 +0300

    alerts when user registers

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 
@@ -18,20 +18,30 @@ import { AngularFireAuth } from 'angularfire2/auth';
 export class RegisterPage {
   @ViewChild('username') user;
   @ViewChild('password') password;
-  constructor(private fire: AngularFireAuth ,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private alertCtrl: AlertController,private fire: AngularFireAuth ,public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
   }
+
+  alert(message: string) {
+    this.alertCtrl.create({
+      title: 'Info!',
+      subTitle: message,
+      buttons: ['OK']
+
+    }).present();
+  }
   registerUser() {
     this.fire.auth.createUserWithEmailAndPassword(this.user.value, this.password.value)
     .then(data =>{
       console.log('got data ',data);
-
+      this.alert('Registred account with username: '+this.user.value);
     })
     .catch(error => {
       console.log('got an error', error);
+      this.alert(error.message);
     });
     console.log('would register user with', this.user.value, this.password.value);
 
