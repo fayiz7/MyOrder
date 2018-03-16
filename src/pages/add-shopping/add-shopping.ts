469ca577a25526f3f64add955376315fa099ec18
ENG911 <engineer911@gmail.com>
2018-03-16 19:34:08 +0300

    adding data to firebaseDatabase using observable

--- src/pages/add-shopping/add-shopping.ts
+++ src/pages/add-shopping/add-shopping.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface';
-/**
- * Generated class for the AddShoppingPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { AngularFireDatabase } from 'angularfire2/database'
+import { FirebaseListObservable } from 'angularfire2/database-deprecated';
+
 
 
 @Component({
@@ -15,13 +12,25 @@ import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface
 })
 export class AddShoppingPage {
   shoppingItem = {} as ShoppingItem;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  //obaservable
+  shoppingItemRef$: FirebaseListObservable<ShoppingItem[]>
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private database: AngularFireDatabase) {
+    this.shoppingItemRef$ = this.database.list('shopping-list');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddShoppingPage');
-  }
   addShoppingItem(shoppingItem: ShoppingItem) {
-    console.log(shoppingItem);
+    this.shoppingItemRef$.push({
+      itemName: this.shoppingItem.itemName,
+      itemNumber: Number(this.shoppingItem.itemNumber)
+    });
+
+    //reset shoppingItem
+    shoppingItem = {} as ShoppingItem;
+
+    //nav the user back to shoppingListPage
+    this.navCtrl.pop();
   }
 }
